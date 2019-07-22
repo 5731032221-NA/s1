cccedd9b502b7ab3e18712fff2d1ffb194ab9354
msc-dts <naphol159@gmail.com>
2019-07-22 15:32:36 +0700

    a

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path')
 //const PORT = process.env.PORT || 5000
 //const app = require('express');
 
-const multer = require('multer')
-const MulterAzureStorage = require('multer-azure-blob-storage').MulterAzureStorage;
-//var multer = require('multer')
-//var multerAzure = require('multer-azure')
+//const multer = require('multer')
+//const MulterAzureStorage = require('multer-azure-blob-storage').MulterAzureStorage;
+var multer = require('multer')
+var multerAzure = require('multer-azure')
 // Expose the /upload endpoint
 const app = require('express')();
 //const http = require('http').Server(app);
@@ -36,39 +36,57 @@ app.listen(port, () => {
   // .get('/', (req, res) => res.render('pages/index'))
   // .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const resolveBlobName = (req, file) => {
-    return new Promise((resolve, reject) => {
-        const blobName = yourCustomLogic(req, file);
-        resolve(blobName);
-    });
-};
+// const resolveBlobName = (req, file) => {
+    // return new Promise((resolve, reject) => {
+        // const blobName = yourCustomLogic(req, file);
+        // resolve(blobName);
+    // });
+// };
  
-const resolveMetadata = (req, file) => {
-    return new Promise((resolve, reject) => {
-        const metadata = yourCustomLogic(req, file);
-        resolve(metadata);
-    });
-};
+// const resolveMetadata = (req, file) => {
+    // return new Promise((resolve, reject) => {
+        // const metadata = yourCustomLogic(req, file);
+        // resolve(metadata);
+    // });
+// };
  
-const azureStorage = new MulterAzureStorage({
-    connectionString: 'DefaultEndpointsProtocol=https;AccountName=oneteamblob;AccountKey=qcv7bSwg5vFNZRt1gY9XLPcv6OWKdKakKCj5znpUQRNQTPAOkLbhnCuZpt/1m4Gc9f5tV55x0CEzcVWjCubTaQ==;EndpointSuffix=core.windows.net',
-    accessKey: 'qcv7bSwg5vFNZRt1gY9XLPcv6OWKdKakKCj5znpUQRNQTPAOkLbhnCuZpt/1m4Gc9f5tV55x0CEzcVWjCubTaQ==',
-    accountName: 'oneteamblob',
-    containerName: 'profilepicture',
-    blobName: resolveBlobName,
-    metadata: resolveMetadata,
-    containerAccessLevel: 'blob',
-    urlExpirationTime: 60
-});
+// const azureStorage = new MulterAzureStorage({
+    // connectionString: 'DefaultEndpointsProtocol=https;AccountName=oneteamblob;AccountKey=qcv7bSwg5vFNZRt1gY9XLPcv6OWKdKakKCj5znpUQRNQTPAOkLbhnCuZpt/1m4Gc9f5tV55x0CEzcVWjCubTaQ==;EndpointSuffix=core.windows.net',
+    // accessKey: 'qcv7bSwg5vFNZRt1gY9XLPcv6OWKdKakKCj5znpUQRNQTPAOkLbhnCuZpt/1m4Gc9f5tV55x0CEzcVWjCubTaQ==',
+    // accountName: 'oneteamblob',
+    // containerName: 'profilepicture',
+    // blobName: resolveBlobName,
+    // metadata: resolveMetadata,
+    // containerAccessLevel: 'blob',
+    // urlExpirationTime: 60
+// });
+
+var upload = multer({ 
+  storage: multerAzure({
+    connectionString: 'DefaultEndpointsProtocol=https;AccountName=oneteamblob;AccountKey=qcv7bSwg5vFNZRt1gY9XLPcv6OWKdKakKCj5znpUQRNQTPAOkLbhnCuZpt/1m4Gc9f5tV55x0CEzcVWjCubTaQ==;EndpointSuffix=core.windows.net', //Connection String for azure storage account, this one is prefered if you specified, fallback to account and key if not.
+    account: 'oneteamblob', //The name of the Azure storage account
+    key: 'qcv7bSwg5vFNZRt1gY9XLPcv6OWKdKakKCj5znpUQRNQTPAOkLbhnCuZpt/1m4Gc9f5tV55x0CEzcVWjCubTaQ==', //A key listed under Access keys in the storage account pane
+    container: 'profilepicture'  //Any container name, it will be created if it doesn't exist
+    blobPathResolver: function(req, file, callback){
+      var blobPath = yourMagicLogic(req, file); //Calculate blobPath in your own way.
+      callback(null, blobPath);
+    }
+  })
+})
+ 
+app.post('/upload', upload.any(), function (req, res, next) {
+  console.log(req.files)
+  res.status(200).send('Uploaded: ' + req.files)
+})
  
 const upload = multer({
     storage: azureStorage
 });
  
-app.post('/upload', upload.single('photo'), (req, res, next) => {
-  console.log(req.files)
-  res.status(200).json(req.files)
-});
+// app.post('/upload', upload.single('photo'), (req, res, next) => {
+  // console.log(req.files)
+  // res.status(200).json(req.files)
+// });
 
 app.get('/',  function (req, res) {
   res.send('Hello Heroo')

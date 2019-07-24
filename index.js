5d8b852794cf7fc45540a42a05f47e91f7068f6e
msc-dts <naphol159@gmail.com>
2019-07-24 17:13:25 +0700

    a

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.listen(port, () => {
     // containerAccessLevel: 'blob',
     // urlExpirationTime: 60
 // });
-var name = 'name';
+var blobPath = 'name';
 
 var upload = multer({ 
   storage: multerAzure({
@@ -69,7 +69,7 @@ var upload = multer({
     key: 'qcv7bSwg5vFNZRt1gY9XLPcv6OWKdKakKCj5znpUQRNQTPAOkLbhnCuZpt/1m4Gc9f5tV55x0CEzcVWjCubTaQ==', //A key listed under Access keys in the storage account pane
     container: 'profilepicture',  //Any container name, it will be created if it doesn't exist
     blobPathResolver: function(req, file, callback){
-      var blobPath = name + '.png'; //Calculate blobPath in your own way.
+      blobPath = Date.now().toString() + '.png'; //Calculate blobPath in your own way.
       callback(null, blobPath);
     }
   })
@@ -77,8 +77,7 @@ var upload = multer({
  
 app.post('/upload', upload.any(), function (req, res, next) {
   console.log(req.files)
-  name = Date.now().toString();
-  res.status(200).send('Uploaded: ' + name)
+  res.status(200).send('Uploaded: ' + blobPath)
 })
  
 // const upload = multer({

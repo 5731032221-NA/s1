d861b125c9e275f43f625a14eb7fb7eeedb41d8a
msc-dts <naphol159@gmail.com>
2019-10-25 15:37:08 +0700

    a

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ var upload = multer({
  
 app.post('/upload', upload.any(), function (req, res, next) {
   console.log(req.files);
-  res.status(200).send("hey");
+  res.status(300).send("hey");
 })
  
 // const upload = multer({
@@ -91,5 +91,5 @@ app.post('/upload', upload.any(), function (req, res, next) {
 // });
 
 app.get('/',  function (req, res) {
-  res.send('Hello Heroo v4')
+  res.send('Hello Heroo v5')
 })
